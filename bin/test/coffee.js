6e5b7e9a87cd858ed2c110e4e2b9b513c688d78e
monsterkodi <monsterkodi@gmx.net>
2019-05-28 21:09:52 +0200

    nerdy

---
+++ bin/test/coffee.js
@@ -0,0 +1,5 @@
+// koffee 0.50.0
+
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29mZmVlLmpzIiwic291cmNlUm9vdCI6Ii4iLCJzb3VyY2VzIjpbIiJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlc0NvbnRlbnQiOlsiIl19
+//# sourceURL=coffee.coffee
